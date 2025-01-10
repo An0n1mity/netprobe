5a52ffc65ec05e09df39614fa65f3418db32447b
monthouel <monthouel@et.esiea.fr>
2025-01-10 05:34:52 -0400

    Refactor NetProbe.sh to use Docker Compose for container management and add error handling for missing compose file

--- NetProbe.sh
+++ NetProbe.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 HOST_VOLUME_PATH="$SCRIPT_DIR/Output"
 CONTAINER_VOLUME_PATH="/usr/src/app/Output"
+COMPOSE_FILE="$SCRIPT_DIR/docker-compose.yml"
 cd $SCRIPT_DIR
 
 # Fonction pour détecter la méthode de pare-feu à utiliser
@@ -49,25 +50,19 @@ unblock_outgoing() {
 
 # Fonction pour gérer l'image et le conteneur Docker
 manage_docker() {
-    IMAGE_NAME="netprobe"
 
-    # Vérifie si l'image existe
-    if ! docker image inspect $IMAGE_NAME &>/dev/null; then
-        echo -e "\n$IMAGE_NAME image do not exist. Creation in progress..."
-
-        docker build -t $IMAGE_NAME -f $SCRIPT_DIR/Dockerfile . &&
-        echo -e "\nImage $IMAGE_NAME created." &&
-        echo 'Starting NetProbe...'
-        docker run --name $IMAGE_NAME-container --network host --privileged --cpus="4" -v "$HOST_VOLUME_PATH:$CONTAINER_VOLUME_PATH" -d $IMAGE_NAME &>/dev/null
-        docker exec -it netprobe-container ip link set enp0s2 promisc
-        echo -e '\nNetProbe started'
-    else
-        docker rm -f $IMAGE_NAME-container &>/dev/null
-        echo -e "\n$IMAGE_NAME image already exist. Starting container..."
-        docker run --name $IMAGE_NAME-container --network host --privileged --cpus="4" -v "$HOST_VOLUME_PATH:$CONTAINER_VOLUME_PATH" -d $IMAGE_NAME &>/dev/null
-        docker exec -it netprobe-container ip link set enp0s2 promisc
-        echo -e '\nNetProbe started.'
+    # Vérifie si le fichier docker-compose.yml existe
+    if [ ! -f "$COMPOSE_FILE" ]; then
+        echo -e "\nDocker Compose file not found. Please ensure $COMPOSE_FILE exists."
+        exit 1
     fi
+
+    echo -e "\nStarting NetProbe using Docker Compose..."
+    docker compose -f $COMPOSE_FILE up -d --build
+
+    # Set the network interface to promiscuous mode
+    docker compose -f $COMPOSE_FILE exec netprobe ip link set enp0s2 promisc
+    echo -e '\nNetProbe started.'
 }
 
 # Fonction principale pour interagir avec l'utilisateur
@@ -94,20 +89,19 @@ To use NetProbe, following paquets must be installed:
                 ;;
             d)
             	echo -e "\nRemove NetProbe container and image..."
-            	docker remove netprobe-container
-            	docker rmi netprobe
+            	docker compose down
             	echo -e "\nNetProbe container and image removed successfully."
             	;;
             u)
                 unblock_outgoing
                 ;;
             n)
-            	block_outgoing
+            	#block_outgoing
                 manage_docker
                 ;;
             q)
             	echo -e "\nStopping NetProbe ..."
-            	docker stop netprobe-container &>/dev/null
+            	docker compose stop
             	unblock_outgoing
                 break
                 ;;
@@ -115,7 +109,7 @@ To use NetProbe, following paquets must be installed:
             	echo -e "\nInstalling dependency ..."
             	sudo -u $SUDO_USER pip install -r requirements.txt
             	echo -e "\nDependency installed. \nCreating report ..."
-            	docker exec -it netprobe-container kill -SIGUSR1 1
+                docker compose exec netprobe kill -SIGUSR1 1
              	sudo -u $SUDO_USER python3 $SCRIPT_DIR/Rapport/Generate-Report.py $SCRIPT_DIR/Output/hosts.json $SCRIPT_DIR/Output/Report  # Il faudra vérifier le path
              	echo -e "\nReport created."
              	;;
