47132c1d6db0d9602321e0b0bd4ff229307de172
67scaryboy <114402859+67scaryboy@users.noreply.github.com>
2025-01-10 08:57:37 +0100

    Script de gestion
    Script de gestion de la sonde

--- NetProbe.sh
+++ NetProbe.sh
@@ -49,20 +49,25 @@ unblock_outgoing() {
 
 # Fonction pour gérer l'image et le conteneur Docker
 manage_docker() {
-    COMPOSE_FILE="$SCRIPT_DIR/docker-compose.yml"
+    IMAGE_NAME="netprobe"
 
-    # Vérifie si le fichier docker-compose.yml existe
-    if [ ! -f "$COMPOSE_FILE" ]; then
-        echo -e "\nDocker Compose file not found. Please ensure $COMPOSE_FILE exists."
-        exit 1
+    # Vérifie si l'image existe
+    if ! docker image inspect $IMAGE_NAME &>/dev/null; then
+        echo -e "\n$IMAGE_NAME image do not exist. Creation in progress..."
+
+        docker build -t $IMAGE_NAME -f $SCRIPT_DIR/Dockerfile . &&
+        echo -e "\nImage $IMAGE_NAME created." &&
+        echo 'Starting NetProbe...'
+        docker run --name $IMAGE_NAME-container --network host --privileged --cpus="4" -v "$HOST_VOLUME_PATH:$CONTAINER_VOLUME_PATH" -d $IMAGE_NAME &>/dev/null
+        docker exec -it netprobe-container ip link set enp0s2 promisc
+        echo -e '\nNetProbe started'
+    else
+        docker rm -f $IMAGE_NAME-container &>/dev/null
+        echo -e "\n$IMAGE_NAME image already exist. Starting container..."
+        docker run --name $IMAGE_NAME-container --network host --privileged --cpus="4" -v "$HOST_VOLUME_PATH:$CONTAINER_VOLUME_PATH" -d $IMAGE_NAME &>/dev/null
+        docker exec -it netprobe-container ip link set enp0s2 promisc
+        echo -e '\nNetProbe started.'
     fi
-
-    echo -e "\nStarting NetProbe using Docker Compose..."
-    docker compose -f $COMPOSE_FILE up -d --build
-
-    # Set the network interface to promiscuous mode
-    docker compose -f $COMPOSE_FILE exec netprobe ip link set enp0s2 promisc
-    echo -e '\nNetProbe started.'
 }
 
 # Fonction principale pour interagir avec l'utilisateur
@@ -97,7 +102,7 @@ To use NetProbe, following paquets must be installed:
                 unblock_outgoing
                 ;;
             n)
-            	#block_outgoing
+            	block_outgoing
                 manage_docker
                 ;;
             q)
@@ -111,7 +116,7 @@ To use NetProbe, following paquets must be installed:
             	sudo -u $SUDO_USER pip install -r requirements.txt
             	echo -e "\nDependency installed. \nCreating report ..."
             	docker exec -it netprobe-container kill -SIGUSR1 1
-             	#sudo -u $SUDO_USER python3 $SCRIPT_DIR/Rapport/Generate-Report.py $SCRIPT_DIR/Output/hosts.json $SCRIPT_DIR/Output/Report  # Il faudra vérifier le path
+             	sudo -u $SUDO_USER python3 $SCRIPT_DIR/Rapport/Generate-Report.py $SCRIPT_DIR/Output/hosts.json $SCRIPT_DIR/Output/Report  # Il faudra vérifier le path
              	echo -e "\nReport created."
              	;;
             *)
